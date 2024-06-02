3cc1033f435ba878d6ac231d73ac99aca2a9cdeb
arustamdev <arustamdev@gmail.com>
2024-06-02 06:45:33 +0200

    Update .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-undef
 module.exports = {
     extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
     parser: '@typescript-eslint/parser',
