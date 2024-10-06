2d63cec8276f68e05a15617cc4a29bacde594b23
arustamdev <arustamdev@gmail.com>
2024-10-06 10:21:17 +0200

    env.ts refactored/improved

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,21 @@
-import { z } from 'zod';
+import { z, ZodError } from 'zod';
 
-const envSchema = z.object({
+const EnvSchema = z.object({
   PORT: z.coerce.number().min(1000),
   NODE_ENV: z.union([z.literal('production'), z.literal('development')]).default('development'),
 });
 
-const env = envSchema.parse(process.env);
+export type env = z.infer<typeof EnvSchema>;
+
+let env: env;
+
+try {
+  env = EnvSchema.parse(process.env);
+} catch (e) {
+  const error = e as ZodError;
+  console.error('Env error:');
+  console.error(error.flatten().fieldErrors);
+  process.exit(1);
+}
 
 export default env;
